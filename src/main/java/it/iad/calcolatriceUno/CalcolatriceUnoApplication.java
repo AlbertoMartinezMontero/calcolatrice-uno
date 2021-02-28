0d0fad68d1ecb7dc1d95b53ff4c400d53bf81f20
Alberto Martinez Montero <martinezmontero.al@gmail.com>
2021-02-28 11:34:08 +0100

    Commit Aggiunta Sring

---
+++ src/main/java/it/iad/calcolatriceUno/CalcolatriceUnoApplication.java
@@ -0,0 +1,13 @@
+package it.iad.calcolatriceUno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcolatriceUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcolatriceUnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/iad/calcolatriceUno/CalcolatriceUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.calcolatriceUno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcolatriceUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
